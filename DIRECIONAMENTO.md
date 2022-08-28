398ce311e0c1a04786306d3a1a7aad0c999c1580
MaurilioSemeao <106936169+MaurilioSemeao@users.noreply.github.com>
2022-08-28 16:03:34 -0300

    adiciona uma função mais limpa para criar elemento na tela   passando função como callback para facilitar na incerção da   verificação e de acerto e erros, adiciona uma função separada   para que rebe parametros para criar uma carta, simplifica e   diminue linhas de codigo,
    	os aruqivos html css img sofreram alterações irrelevantes
    	apenahs modificações de status para teste.
    
    	- javaScript
    	- css
    	- html

--- DIRECIONAMENTO.md
+++ DIRECIONAMENTO.md
@@ -8,4 +8,9 @@
          - Criar um gride para alocar as carta, 
          - Criar carta e adiconar efeito de flip,
       - Adicinar arquivo js,
-         - Criar as catas pelo js e exibir no html,      
+         - Criar as catas pelo js e exibir no html,
+         - criar uma função para exibir a quantidade de acars no html,
+         - Adicionar o  flip as catara,
+         - Criar logica para validação de jogo,
+         - Criar logica para verificr erro e acerto,
+         
--- assets/js/scriptjogo.js
+++ assets/js/scriptjogo.js
@@ -1,20 +1,70 @@
 'unse strict';
 (()=>{
-    function criaCard(){
-        const grid = document.querySelector(".container-grid");
-        const card = document.createElement("div");
-        const faceFront = document.createElement("div");
-        const faceBack = document.createElement("div");
+    const nomePersonagens=[
+        "dragao-alado-de-ra",
+        "dragao-caveira-negro",
+        "dragao-milenar",
+        "dragrao-branco-de-olhos-azuis",
+        "dragrao-negro-dos-olhos-vermelhos",
+        "exodia",
+        "gerreiro-da-espada-flamejante-",
+        "kuriboh",
+        "mago-do-tempo",
+        "mago-negro",
+        // "obelisco-o-atormentador",
+        // "slifer-o-dragao-celeste",
+        // "spellcaster",
+        // "troca-de-coração",
+        // "yami-yugi",
+    ]
+  
+    function Cards(tag, nomeClass){
+        const card = document.createElement(tag);
 
-        card.classList.add("card");
-        faceFront.classList.add("face","front");
-        faceBack.classList.add("face","back");
-        
+        card.className = nomeClass; 
+
+        return card;
+    }
+    
+    function newCard(item, callback){
+        const grid = document.querySelector(".container-grid");
+        const card =  Cards('div', 'card');
+        const faceFront = Cards('div', 'face front' )
+        const faceBack = Cards('div', 'face back')
+
+        card.setAttribute("data-personagen",item)
+        faceFront.style.backgroundImage = `url("../assets/img/${item}.png")`;
+       
         card.appendChild(faceFront);
         card.appendChild(faceBack);
-        
-        grid.appendChild(card)
 
+        callback(card)
+
+        grid.appendChild(card)
+      
+       
     }
-    criaCard();
+
+    function flip(item){
+       if(item.target.parentNode.className === "container-grid" ||item.target.parentNode.className.includes("flip")){
+            return
+        }
+       
+        item.target.parentNode.classList.add("flip")
+        console.log("click")
+    }
+    
+    function iniciaJogo(){
+        const personaDuplicado = [...nomePersonagens, ...nomePersonagens];
+        personaDuplicado.sort(()=> 0.10 - Math.random());
+
+        personaDuplicado.forEach((item)=>{
+            newCard(item,(card)=>{
+                card.addEventListener("click",item=> flip(item));
+            });
+            
+        })
+    }
+    iniciaJogo();
+
 })();
