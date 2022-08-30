bc795350935a6a6618732810af809458b38266a1
MaurilioSemeao <106936169+MaurilioSemeao@users.noreply.github.com>
2022-08-30 00:51:22 -0300

    adiciona direcionamento da pg login pra pg jogo

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,6 +2,7 @@
 (()=>{
     const $input = document.querySelector('.input-name');
     const $button = document.querySelector('.button-login');
+    const $forms = document.forms[0];
 
     function validarInput({target}){
         if(target.value.length > 3){
@@ -12,5 +13,13 @@
     }
 
     $input.addEventListener('input',validarInput)
+
+    $forms.addEventListener('submit',(event)=>{
+        event.preventDefault();
+
+        window.location = "./paginas/jogo.html";
+        window.localStorage.setItem('nome',$input.value);
+        window.sessionStorage.setItem('nome',$input.value);
+    });
 
 })()
